d2f5649b76016e348bf3e057bcc300c4629b993d
Shivam Khattar <56852615+iamkhattar@users.noreply.github.com>
2025-01-10 22:30:20 +0000

    feat: added variable formatting fix

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,6 +2,6 @@
 # or using the -var="hetzner_api_token=..." CLI option
 variable "hetzner_cloud_api_token" {
   description = "Hetzner API token"
-  sensitive = true
-  type = string
+  sensitive   = true
+  type        = string
 }
