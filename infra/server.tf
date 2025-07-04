8f08c43eb1be17bdd82ed9ea0842bd04b46b327d
Shivam Khattar <56852615+iamkhattar@users.noreply.github.com>
2025-07-04 18:44:54 +0100

    feat[#12]: Added k3s ansible playbook

--- infra/server.tf
+++ infra/server.tf
@@ -7,7 +7,14 @@ resource "hcloud_primary_ip" "server_node_public_ip" {
   auto_delete   = true
 }
 
-resource "hcloud_server" "server-node" {
+data "template_file" "server_node_config" {
+  template = file("${path.module}/config/cloud-init-server.yaml")
+  vars = {
+    local_ssh_public_key = file("${path.module}/config/.ssh/id_rsa.pub")
+  }
+}
+
+resource "hcloud_server" "server_node" {
   name        = "server-node-0"
   image       = var.server.image
   server_type = var.server.type
@@ -15,17 +22,19 @@ resource "hcloud_server" "server-node" {
   labels = {
     type: "server"
   }
+
   public_net {
     ipv4         = hcloud_primary_ip.server_node_public_ip.id
     ipv4_enabled = true
     ipv6_enabled = true
   }
+
   network {
     network_id = hcloud_network.private_network.id
     ip         = var.server.ip
   }
 
-  user_data = file("${path.module}/config/cloud-init-server.yaml")
+  user_data = data.template_file.server_node_config.rendered
 
   # If we don't specify this, Terraform will create the resources in parallel
   # We want this node to be created after the private network is created
